cbd7fdc7256c09da28b49ce200927e9edaf44b31
Joseph <Josph@com.pl>
2025-03-11 09:11:14 +0100

    estetyka

--- Ankieta-Preferencji/obj/Debug/net9.0/Ankieta-Preferencji.AssemblyInfo.cs
+++ Ankieta-Preferencji/obj/Debug/net9.0/Ankieta-Preferencji.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ankieta-Preferencji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca2b15d40c55c5b75907a6d33ee2c112ca838e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f04e0123b3caa0d52dde769629553ab5149983")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ankieta-Preferencji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ankieta-Preferencji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
