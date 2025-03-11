fc5c0f845058dd2bfc1bfb9b1cc289a478e7cbce
Joseph <Josph@com.pl>
2025-03-11 08:49:44 +0100

    Wprowadzenie danych uzytkownika

--- Ankieta-Preferencji/MainWindow.axaml.cs
+++ Ankieta-Preferencji/MainWindow.axaml.cs
@@ -1,11 +1,23 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Ankieta_Preferencji;
 
 public partial class MainWindow : Window
 {
+   
     public MainWindow()
     {
         InitializeComponent();
+        
+        
+    }
+    
+    private void SubmitButton_Click(object? sender, RoutedEventArgs e)
+    {
+        var comboBoxValue = (my_ComboBox.SelectedItem as ComboBoxItem)?.Content?.ToString() ?? "No selection";
+        var textBoxValue = TextBox.Text;
+
+        infoLabel.Content = $"{textBoxValue} i jego zainteresowanie to {comboBoxValue}";
     }
 }
--- Ankieta-Preferencji/obj/Debug/net9.0/Ankieta-Preferencji.AssemblyInfo.cs
+++ Ankieta-Preferencji/obj/Debug/net9.0/Ankieta-Preferencji.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ankieta-Preferencji")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca2b15d40c55c5b75907a6d33ee2c112ca838e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ankieta-Preferencji")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ankieta-Preferencji")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
